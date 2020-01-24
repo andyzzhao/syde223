b9cf78a41d1e3e14ae45324247e6d7307a0733e1
Andy <andy.cz.zhao@gmail.com>
2020-01-24 12:25:45 -0500

    lab1

--- lab_assignments/lab1/lab1_polynomial.cpp
+++ lab_assignments/lab1/lab1_polynomial.cpp
@@ -1,2 +1,23 @@
+// GROUP 14
 // Andrew Zhao 20785698
 // Jeffery Ng 20762964
+
+#include <iostream>
+#include <vector>
+
+using namespace std; 
+
+class Polynomial {
+    vector<int> data; // size already stored inside vector
+    Polynomial(int A[], int size) { data.resize(size);
+};
+~Polynomial() { 
+    // data will be deleted on its own 
+}
+
+
+
+int main(){
+
+    return 0;
+}
