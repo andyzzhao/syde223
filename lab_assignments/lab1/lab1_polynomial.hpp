0728f1aebbae658eff89e242af618e35281d2c78
Andy <andy.cz.zhao@gmail.com>
2020-01-30 00:14:34 -0500

    wip

--- lab_assignments/lab1/lab1_polynomial.hpp
+++ lab_assignments/lab1/lab1_polynomial.hpp
@@ -7,25 +7,52 @@ using namespace std;
 
 class Polynomial {
     vector<int> data;
+    int size; 
 
 public:
     Polynomial();
     Polynomial(int A[], int size);
+    Polynomial(string fileName);
 
     // Destructor
     ~Polynomial();
 
+    // Operator
+    bool operator==(const Polynomial& target); // performs *this == target
+    Polynomial operator+(const Polynomial& target); // performs *this + target
+    Polynomial operator-(const Polynomial& target); // performs *this - target
+    Polynomial operator*(const Polynomial& target); // performs *this * target
+
     // Setters and Getters
-    void get_data() {
-        return data;
-    }
+    vector<int> get_data() const;
+
+    // Helper functions
+    Polynomial derivative(); // computers derivative of *this
+    void print();
 };
 
 class PolynomialTest {
 
 public:
-    bool test_constructors1();
+    // constructor tests
+    bool test_constructor1();
+    bool test_constructor2();
+    bool test_constructor3();
+
+    // operator tests
+    bool test_operator1();
+    bool test_operator2();
+    bool test_operator3();
+    bool test_operator4();
+
+    // getter test
+    bool test_getter();
+
+    // helper function tests
+    bool test_derivative();
+    bool test_print();
+
     void run();
-}
+};
 
 #endif
